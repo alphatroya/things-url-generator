804e037c1b568259430d6d33f80ea1bd30dc5790
Alexey Korolev <alphatroya@gmail.com>
2021-01-18 16:27:14 +0300

    Add ability to set notes for a checklist project (#2)

--- Sources/things-url-generator/main.swift
+++ Sources/things-url-generator/main.swift
@@ -26,17 +26,18 @@ struct ThingsUrlGenerator: ParsableCommand {
         Command receives data from the stdin channel and sends add-project Things deeplink URL to stdout.
 
         EXAMPLE:
-        echo '# Test project\\n- [ ] first item\\n- [ ] second item' | things-url-generator
-        'things:///add-project?title=Test%20project&to-dos=%20first%20item&reveal=true'
+        > echo '# Test project\\nNotes\\n- [ ] first item\\n- [ ] second item' | things-url-generator
+        things:///add-project?title=Test%20project&notes=Notes&to-dos=%20first%20item%0A%20second%20item&reveal=true
 
         By opening that link, a new project named "Test project" will be added and revealed in the Things app.
-        The created project will contain two to-do items.
+        The created project will contain a note description "Notes" and two to-do items.
         """,
         version: "0.0.1"
     )
 
     mutating func run() throws {
         var title: String?
+        var noteLines: [String] = []
         var todo = [String]()
         while var line = readLine()?.trimmingCharacters(in: .whitespaces) {
             if line.isEmpty {
@@ -53,7 +54,11 @@ struct ThingsUrlGenerator: ParsableCommand {
                 title = line
                 continue
             }
-            guard line.deletingPrefix("- [ ]") else {
+            if !line.deletingPrefix("- [ ]") {
+                if todo.isEmpty {
+                    noteLines.append(line)
+                    continue
+                }
                 print("todo item should begin with - [ ]", to: &stdErr)
                 throw ExitCode.failure
             }
@@ -70,6 +75,9 @@ struct ThingsUrlGenerator: ParsableCommand {
         components.host = ""
         components.path = "/add-project"
         var query = [URLQueryItem(name: "title", value: title)]
+        if !noteLines.isEmpty {
+            query.append(URLQueryItem(name: "notes", value: noteLines.joined(separator: "\n")))
+        }
         if !todo.isEmpty {
             query.append(URLQueryItem(name: "to-dos", value: todo.joined(separator: "\n")))
         }
